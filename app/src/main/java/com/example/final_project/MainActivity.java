3234cb451f34f26d4f4fc3850bd6e6f9fb2a754f
rrTio <tio.rozsraven@ue.edu.ph>
2021-05-01 00:13:38 +0800

    Added trash images

--- app/src/main/java/com/example/final_project/MainActivity.java
+++ app/src/main/java/com/example/final_project/MainActivity.java
@@ -15,7 +15,7 @@ public class MainActivity extends AppCompatActivity
 {
     Drawable battery, bulbs, eWaste, glass, metal, organic, paper, plastic;
     ImageButton ibtnBattery, ibtnBulbs, ibtnEWaste, ibtnGlass, ibtnMetal, ibtnOrganic, ibtnPaper, ibtnPlastic;
-    ImageView player;
+    ImageView player, trash;
     TextView lblScore, trashName;
 
     int score = 0;
@@ -52,51 +52,67 @@ public class MainActivity extends AppCompatActivity
         });
 
         //CHANGE PLAYER'S IMAGE & SET THE BIN'S NAME
-        ibtnBattery.setOnClickListener(new View.OnClickListener()   {@Override public void onClick(View v) {player.setImageDrawable(battery); trashName.setText(strbatteries);}});
-        ibtnBulbs.setOnClickListener(new View.OnClickListener()     {@Override public void onClick(View v) {player.setImageDrawable(bulbs);   trashName.setText(strbulbs);}});
-        ibtnEWaste.setOnClickListener(new View.OnClickListener()    {@Override public void onClick(View v) {player.setImageDrawable(eWaste);  trashName.setText(strEWaste);}});
-        ibtnGlass.setOnClickListener(new View.OnClickListener()     {@Override public void onClick(View v) {player.setImageDrawable(glass);   trashName.setText(strGlass);}});
-        ibtnMetal.setOnClickListener(new View.OnClickListener()     {@Override public void onClick(View v) {player.setImageDrawable(metal);   trashName.setText(strMetals);}});
-        ibtnOrganic.setOnClickListener(new View.OnClickListener()   {@Override public void onClick(View v) {player.setImageDrawable(organic); trashName.setText(strOrganic);}});
-        ibtnPaper.setOnClickListener(new View.OnClickListener()     {@Override public void onClick(View v) {player.setImageDrawable(paper);   trashName.setText(strPapers);}});
-        ibtnPlastic.setOnClickListener(new View.OnClickListener()   {@Override public void onClick(View v) {player.setImageDrawable(plastic); trashName.setText(strPlastics);}});
+        ibtnBattery.setOnClickListener(new View.OnClickListener()
+        {@Override public void onClick(View v) {player.setImageDrawable(battery); trashName.setText(strbatteries);}});
+
+        ibtnBulbs.setOnClickListener(new View.OnClickListener()
+        {@Override public void onClick(View v) {player.setImageDrawable(bulbs);   trashName.setText(strbulbs);}});
+
+        ibtnEWaste.setOnClickListener(new View.OnClickListener()
+        {@Override public void onClick(View v) {player.setImageDrawable(eWaste);  trashName.setText(strEWaste);}});
+
+        ibtnGlass.setOnClickListener(new View.OnClickListener()
+        {@Override public void onClick(View v) {player.setImageDrawable(glass);   trashName.setText(strGlass);}});
+
+        ibtnMetal.setOnClickListener(new View.OnClickListener()
+        {@Override public void onClick(View v) {player.setImageDrawable(metal);   trashName.setText(strMetals);}});
+
+        ibtnOrganic.setOnClickListener(new View.OnClickListener()
+        {@Override public void onClick(View v) {player.setImageDrawable(organic); trashName.setText(strOrganic);}});
+
+        ibtnPaper.setOnClickListener(new View.OnClickListener()
+        {@Override public void onClick(View v) {player.setImageDrawable(paper);   trashName.setText(strPapers);}});
+
+        ibtnPlastic.setOnClickListener(new View.OnClickListener()
+        {@Override public void onClick(View v) {player.setImageDrawable(plastic); trashName.setText(strPlastics);}});
     }
 
     //SETUP COMPONENTS
     public void setup()
     {
         //INSTANTIATION
-        ibtnBattery = findViewById(R.id.ibtn_battery);
         ibtnBulbs = findViewById(R.id.ibtn_bulbs);
-        ibtnEWaste = findViewById(R.id.ibtn_ewaste);
         ibtnGlass = findViewById(R.id.ibtn_glass);
         ibtnMetal = findViewById(R.id.ibtn_metal);
-        ibtnOrganic = findViewById(R.id.ibtn_organic);
         ibtnPaper = findViewById(R.id.ibtn_paper);
+        ibtnEWaste = findViewById(R.id.ibtn_ewaste);
+        ibtnBattery = findViewById(R.id.ibtn_battery);
+        ibtnOrganic = findViewById(R.id.ibtn_organic);
         ibtnPlastic = findViewById(R.id.ibtn_plastic);
 
-        battery = ibtnBattery.getDrawable();
         bulbs = ibtnBulbs.getDrawable();
-        eWaste = ibtnEWaste.getDrawable();
         glass = ibtnGlass.getDrawable();
         metal = ibtnMetal.getDrawable();
-        organic = ibtnOrganic.getDrawable();
         paper = ibtnPaper.getDrawable();
+        eWaste = ibtnEWaste.getDrawable();
+        battery = ibtnBattery.getDrawable();
+        organic = ibtnOrganic.getDrawable();
         plastic = ibtnPlastic.getDrawable();
 
+        trash = findViewById(R.id.trash);
         player = findViewById(R.id.player);
 
         trashName = findViewById(R.id.trashName);
         lblScore = findViewById(R.id.lblScore);
         lblScore.setText("Score: " + score);
 
-        strbatteries = "BATTERIES";
         strbulbs = "BULBS";
-        strEWaste = "E-WASTES";
+        strPapers = "PAPERS";
         strGlass = "GLASSES";
         strMetals = "METALS";
+        strEWaste = "E-WASTES";
         strOrganic = "ORGANICS";
-        strPapers = "PAPERS";
         strPlastics = "PLASTICS";
+        strbatteries = "BATTERIES";
     }
 }
