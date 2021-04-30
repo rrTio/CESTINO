b00800c1746450f29f802bca49e96637c11e5f52
rrTio <tio.rozsraven@ue.edu.ph>
2021-04-30 21:39:57 +0800

    Made "Player" object movable

--- app/src/main/java/com/example/final_project/MainActivity.java
+++ app/src/main/java/com/example/final_project/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.final_project;
 
+import android.view.MotionEvent;
+import android.view.View;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,12 +11,50 @@ public class MainActivity extends AppCompatActivity
 {
     ImageButton ibtnBattery, ibtnBulbs, ibtnEWaste, ibtnGlass, ibtnMetal, ibtnOrganic, ibtnPaper, ibtnPlastic;
     ImageView player;
+    float xDown = 0,yDown = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         setup();
+
+        player.setOnTouchListener(new View.OnTouchListener()
+        {
+            @Override
+            public boolean onTouch(View v, MotionEvent event)
+            {
+                switch (event.getActionMasked())
+                {
+                    //USER'S TOUCH
+                    case MotionEvent.ACTION_DOWN:
+
+                        xDown = event.getX();
+                        yDown = event.getY();
+
+                        break;
+
+                    //USER'S MOTION
+                    case MotionEvent.ACTION_MOVE:
+                        float movedX, movedY, distanceX, distanceY;
+
+                        movedX = event.getX();
+                        movedY = event.getY();
+
+                        distanceX = movedX - xDown;
+                        distanceY = movedY - yDown;
+
+                        player.setX(player.getX() + distanceX);
+                        player.setY(player.getY() + distanceY);
+                        xDown=movedX;
+                        yDown=movedY;
+
+                        break;
+                }
+
+                return true;
+            }
+        });
     }
 
     public void setup()
